521754fbddd682c077e4e8f2e331fe94900b94d5
Patrick <patrick.mell@stud.tu-darmstadt.de>
2018-12-29 20:38:22 +0100

    Added an analysis which collects which methods of a class are called within a project and how often.

---
+++ DEVELOPING_OPAL/tools/src/main/scala/org/opalj/support/info/ClassUsageAnalysis.scala
@@ -0,0 +1,121 @@
+/* BSD 2-Clause License - see OPAL/LICENSE for details. */
+package org.opalj.support.info
+
+import java.net.URL
+
+import org.opalj.br.analyses.BasicReport
+import org.opalj.br.analyses.DefaultOneStepAnalysis
+import org.opalj.br.analyses.Project
+import org.opalj.br.analyses.ReportableAnalysisResult
+import org.opalj.fpcf.analyses.string_definition.V
+import org.opalj.tac.Assignment
+import org.opalj.tac.Call
+import org.opalj.tac.ExprStmt
+import org.opalj.tac.SimpleTACAIKey
+import org.opalj.tac.VirtualFunctionCall
+
+import scala.annotation.switch
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Analyzes a project for how a particular class is used within that project. This means that this
+ * analysis collect information on which methods are called on objects of that class as well as how
+ * often.
+ *
+ * Currently, the analysis can be configured by setting the two object variables
+ * [[ClassUsageAnalysis.className]] and [[ClassUsageAnalysis.isFineGrainedAnalysis]].
+ *
+ * @author Patrick Mell
+ */
+object ClassUsageAnalysis extends DefaultOneStepAnalysis {
+
+    override def title: String = "Class Usage Analysis"
+
+    override def description: String = {
+        "Analysis a project for how a particular class is used, i.e., which methods are called "+
+            "on it"
+    }
+
+    /**
+     * The fully-qualified name of the class that is to be analyzed in a Java format, i.e., dots as
+     * package / class separators.
+     */
+    private val className = "java.lang.StringBuilder"
+
+    /**
+     * The analysis can run in two modes: Fine-grained or coarse-grained. Fine-grained means that
+     * two method are considered as the same only if their method descriptor is the same, i.e., this
+     * mode enables a differentiation between overloaded methods.
+     * The coarse-grained method, however, regards two method calls as the same if the class of the
+     * base object as well as the method name are equal, i.e., overloaded methods are not
+     * distinguished.
+     */
+    private val isFineGrainedAnalysis = true
+
+    /**
+     * Takes a [[Call]] and assembles the method descriptor for this call. The granularity is
+     * determined by [[isFineGrainedAnalysis]]: For a fine-grained analysis, the returned string has
+     * the format "[fully-qualified classname]#[method name]: [stringified method descriptor]" and
+     * for a coarse-grained analysis: [fully-qualified classname]#[method name].
+     */
+    private def assembleMethodDescriptor(call: Call[V]): String = {
+        val fqMethodName = s"${call.declaringClass.toJava}#${call.name}"
+        if (isFineGrainedAnalysis) {
+            val methodDescriptor = call.descriptor.toString
+            s"$fqMethodName: $methodDescriptor"
+        } else {
+            fqMethodName
+        }
+    }
+
+    /**
+     * Takes any function [[Call]], checks whether the base object is of type [[className]] and if
+     * so, updates the passed map by adding the count of the corresponding method. The granularity
+     * for counting is determined by [[isFineGrainedAnalysis]].
+     */
+    private def processFunctionCall(call: Call[V], map: mutable.Map[String, Int]): Unit = {
+        val declaringClassName = call.declaringClass.toJava
+        if (declaringClassName == className) {
+            val methodDescriptor = assembleMethodDescriptor(call)
+            if (map.contains(methodDescriptor)) {
+                map(methodDescriptor) += 1
+            } else {
+                map(methodDescriptor) = 1
+            }
+        }
+    }
+
+    override def doAnalyze(
+        project: Project[URL], parameters: Seq[String], isInterrupted: () ⇒ Boolean
+    ): ReportableAnalysisResult = {
+        val resultMap = mutable.Map[String, Int]()
+        val tacProvider = project.get(SimpleTACAIKey)
+
+        project.allMethodsWithBody.foreach { m ⇒
+            tacProvider(m).stmts.foreach { stmt ⇒
+                (stmt.astID: @switch) match {
+                    case Assignment.ASTID ⇒ stmt match {
+                        case Assignment(_, _, c: VirtualFunctionCall[V]) ⇒
+                            processFunctionCall(c, resultMap)
+                        case _ ⇒
+                    }
+                    case ExprStmt.ASTID ⇒ stmt match {
+                        case ExprStmt(_, c: VirtualFunctionCall[V]) ⇒
+                            processFunctionCall(c, resultMap)
+                        case _ ⇒
+                    }
+                    case _ ⇒
+                }
+            }
+        }
+
+        val report = ListBuffer[String]("Results")
+        // Transform to a list, sort in ascending order of occurrences and format the information
+        report.appendAll(resultMap.toList.sortWith(_._2 < _._2).map {
+            case (descriptor: String, count: Int) ⇒ s"$descriptor: $count"
+        })
+        BasicReport(report)
+    }
+
+}
